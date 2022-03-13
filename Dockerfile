11a1a79dae466960e6b61770040579b5cf2d73e7
Florian Sihler <florian.sihler@uni-ulm.de>
2022-03-13 23:05:43 +0100

    clean up run of agec

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ ENV AGEC_HOME="$HOME/agec-master"
 
 RUN addgroup --gid 1000 agec-user && adduser --uid 1000 --ingroup agec-user --home "$HOME" --disabled-password agec-user
 
-RUN apk add --update --no-cache bash wget unzip python2
+RUN apk add --update --no-cache bash wget unzip python2 sed
 RUN python2 -m ensurepip
 
 WORKDIR "$HOME"
@@ -18,11 +18,16 @@ RUN rm -rf master.zip
 COPY test "$HOME/test"
 COPY run_agec.sh /
 
+# linkin
+RUN ln -s "$(which javap)" "/usr/bin/javap" # path is hardcoded
+
 RUN chmod +x /run_agec.sh && /run_agec.sh "$HOME/test"
 RUN diff -w "$HOME/clone_index.txt" "$HOME/test/expected_index.txt"
 RUN diff -w "$HOME/ngram.txt" "$HOME/test/expected_ngram.txt"
 RUN diff -w "$HOME/clones.txt" "$HOME/test/expected_linenum.txt"
 RUN diff -w "$HOME/exp_clones.txt" "$HOME/test/expected_cdt_index.txt"
+RUN rm -rf "$HOME/disassembled" "$HOME/classlist.lst" "$HOME/Classify.jar"
+# The results
 RUN rm -rf "$HOME/clones.txt" "$HOME/ngram.txt" "$HOME/clone_index.txt" "$HOME/exp_clones.txt" "$HOME/test"
 
 USER agec-user
--- run_agec.sh
+++ run_agec.sh
@@ -1,20 +1,33 @@
 #!/usr/bin/env bash
 BUILD_DIR=../build
+echo "Running on $1"
 cd "$1"
 
 echo "Step 1: produce and disassemble java jar files"
-echo "     1.1: comile java files"
-javac -d "$BUILD_DIR" *.java
+echo "     1.1: prepare all java files"
+# we do have a major problem. The java files have to compile. Although they are from different people in
+# different folders and packages, using different java versions and which may reference other files
+# in the same folder
+
+# first of all, we remove *all* package statements:
+# for f in $(find -name "*.java"); do
+#    sed 's/package .*;//' -i $f
+# done
+
+echo "     1.2: compile java files"
+echo $(find -name "*.java")
+javac -d "$BUILD_DIR" $(find -name "*.java")
 # undocumented :/
-echo "     1.2: produce classlist"
+echo "     1.3: produce classlist"
 cd "$BUILD_DIR"
-ls > "$HOME/classlist.lst"
-echo "     1.3: produce jar"
-jar cvf Classify.jar *
-echo "     1.4: link javap for disassembler"
-ln -s "$(which javap)" "/usr/bin/javap" # path is hardcoded
+ls *.class > "$HOME/classlist.lst"
+echo "     1.4: produce jar"
+set -eu
+jar cvf "$HOME/Classify.jar" *
+
+
 echo "     1.5: disassemble jar"
-python2 "$AGEC_HOME/src/run_disasm.py" Classify.jar "$HOME/classlist.lst" -o "$HOME/disassembled"
+python2 "$AGEC_HOME/src/run_disasm.py" "$HOME/Classify.jar" "$HOME/classlist.lst" -o "$HOME/disassembled"
 
 echo "Step 2: Generate ngram"
 python2 "$AGEC_HOME/src/gen_ngram.py" --asm-directory "$HOME/disassembled" > "$HOME/ngram.txt"
@@ -27,6 +40,9 @@ python2 "$AGEC_HOME/src/tosl_clone.py" --asm-directory "$HOME/disassembled" "$HO
 
 echo "Step 5: Calculate clone metrics"
 python2 "$AGEC_HOME/src/exp_clone.py" --asm-directory "$HOME/disassembled" "$HOME/clone_index.txt" --loc-to-trace --add-metric-clat --add-metric-max-depth > "$HOME/exp_clones.txt"
+
+echo "Step 6: Cleanup"
+rm -rf "$BUILD_DIR"
 
 echo "=====Clones detected:====="
 cat "$HOME/clones.txt"
